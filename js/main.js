e916f9e5792869201f38a00ecc8db44afddc585e
SergeyZheleznov <jeleznov_sv@mail.ru>
2021-02-16 20:06:29 +0300

    Правка 1 задание 2 дата16.02.2021

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-TEST
+/* Функция, возвращающая случайное целое число из переданного диапазона включительно.*/
+//https://habr.com/ru/company/ruvds/blog/534108/
+alert('я подключил скрипт');
+
+function randomInteger(min,max) {
+  if (min > max) {
+    alert('Произошла ошибка при вводе диапазона, минимальное значение больше максимального, введете диапазон правильно');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let min = 10;
+let max = 20;
+let res = randomInteger(min,max);
+
+alert(res);
+
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+
+function randomDig(min, max, numbersAfterComma) {
+  res2 = Math.random() * (max - min) + min;
+  return res2.toFixed(numbersAfterComma);
+}
+
+min = 12;
+max = 18;
+let numbersAfterComma = 3;
+let res2;
+let res3 = randomDig(min, max, numbersAfterComma);
+
+alert(res3);
